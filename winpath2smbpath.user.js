24aa56bcd783a9916622d810615e6035a3e5b269
abicky <takeshi.arabiki@gmail.com>
2011-06-10 21:58:46 +0900

    winpath2smbpath.user.js
    * use setInterval to reload in case that the iframe hasn't been loaded yet

--- winpath2smbpath.user.js
+++ winpath2smbpath.user.js
@@ -5,20 +5,27 @@
 // @description    Convert Windows path to Unix path in Gmail
 // ==/UserScript==
 
-(function() {
-        var mailframe = document.getElementById('canvas_frame');
-        mailframe.contentDocument.addEventListener('click', function() {
-            var mails = this.getElementsByClassName('gs');
-            for (var i = 0; i < mails.length; i++) {
-                var body = mails[i].getElementsByClassName('ii gt');
-                for (var j = 0; j < body.length; j++) {
-                    body[j].innerHTML = body[j].innerHTML.replace(/\\\\(?:[^\s<]|<wbr>)+/,
-                                                                  function(path) {
-                                                                      path = path.replace(/<.*?>/g, '');
-                                                                      path = 'smb:' + path.replace(/\\/g, '/');
-                                                                      return '<a href="' + path + '">' + path + '</a>';
-                                                                  });
-                }
+function winpath2smbpath() {
+    var mailframe = document.getElementById('canvas_frame');
+    if (mailframe != undefined) {
+        clearInterval(timer);
+    } else {
+        return;
+    }
+    mailframe.contentDocument.addEventListener('click', function() {
+        var mails = this.getElementsByClassName('gs');
+        for (var i = 0; i < mails.length; i++) {
+            var body = mails[i].getElementsByClassName('ii gt');
+            for (var j = 0; j < body.length; j++) {
+                body[j].innerHTML = body[j].innerHTML.replace(/\\\\(?:[^\s<]|<wbr>)+/,
+                                                              function(path) {
+                                                                  path = path.replace(/<.*?>/g, '');
+                                                                  path = 'smb:' + path.replace(/\\/g, '/');
+                                                                  return '<a href="' + path + '">' + path + '</a>';
+                                                              });
             }
-        }, false);
-})();
+        }
+    }, false);
+}
+
+var timer = setInterval(winpath2smbpath, 1000);
