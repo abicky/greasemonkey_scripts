1dbd6c7df39c1803ae2e1224e5721d537670616d
abicky <takeshi.arabiki@gmail.com>
2011-06-13 23:24:18 +0900

    winpath2smbpath.user.js
    * use setTimeout instead of setInterval because the script sometime don't work for some reason.
      'return;' may be the cause of the trouble because 'return 1;' work well.

--- winpath2smbpath.user.js
+++ winpath2smbpath.user.js
@@ -2,30 +2,29 @@
 // @name           Windows Path 2 SMB Path
 // @namespace      http://abicky.site90.com/
 // @include        https://mail.google.com/mail/*#*
-// @description    Convert Windows path to Unix path in Gmail
+// @description    Convert Windows path (UNC) to SMB path in Gmail
 // ==/UserScript==
 
 function winpath2smbpath() {
     var mailframe = document.getElementById('canvas_frame');
-    if (mailframe != undefined) {
-        clearInterval(timer);
-    } else {
-        return;
-    }
-    mailframe.contentDocument.addEventListener('click', function() {
-        var mails = this.getElementsByClassName('gs');
-        for (var i = 0; i < mails.length; i++) {
-            var body = mails[i].getElementsByClassName('ii gt');
-            for (var j = 0; j < body.length; j++) {
-                body[j].innerHTML = body[j].innerHTML.replace(/\\\\(?:[^\s<]|<wbr>)+/,
-                                                              function(path) {
-                                                                  path = path.replace(/<.*?>/g, '');
-                                                                  path = 'smb:' + path.replace(/\\/g, '/');
-                                                                  return '<a href="' + path + '">' + path + '</a>';
-                                                              });
+    if (mailframe != null) {
+        mailframe.contentDocument.addEventListener('click', function() {
+            var mails = this.getElementsByClassName('gs');
+            for (var i = 0; i < mails.length; i++) {
+                var body = mails[i].getElementsByClassName('ii gt');
+                for (var j = 0; j < body.length; j++) {
+                    body[j].innerHTML = body[j].innerHTML.replace(/\\\\(?:[^\s<]|<wbr>)+/,
+                                                                  function(path) {
+                                                                      path = path.replace(/<.*?>/g, '');
+                                                                      path = 'smb:' + path.replace(/\\/g, '/');
+                                                                      return '<a href="' + path + '">' + path + '</a>';
+                                                                  });
+                }
             }
-        }
-    }, false);
+        }, false);
+    } else {
+        setTimeout(winpath2smbpath, 1000);
+    }
 }
 
-var timer = setInterval(winpath2smbpath, 1000);
+setTimeout(winpath2smbpath, 1000);
